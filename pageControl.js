79e4b4b81c21c74ff762e7f0a37870a29e248aca
Dilan Patel <dilanpace1496@gmail.com>
2018-11-27 12:06:16 -0500

    fix to js?

--- pageControl.js
+++ pageControl.js
@@ -6,13 +6,13 @@ $(function() {
 			var parentOffset = $(this).offset(),
       		relX = e.pageX - parentOffset.left,
       		relY = e.pageY - parentOffset.top;
-			$(this).find('span').css({top:relY, left:relX})
+			$(this).find('button').css({top:relY, left:relX})
     })
     .on('mouseout', function(e) {
 			var parentOffset = $(this).offset(),
       		relX = e.pageX - parentOffset.left,
       		relY = e.pageY - parentOffset.top;
-    	$(this).find('span').css({top:relY, left:relX})
+    	$(this).find('button').css({top:relY, left:relX})
     });
   $('[href=#]').click(function(){return false});
 });
