e31b7b3db9642b6b0066e2b858ae74b9b4eb6f00
Matt-the-Marxist <m.morrissette105000@gmail.com>
2018-12-20 11:27:08 -0500

    Update pages.js

--- new/static/js/pages.js
+++ new/static/js/pages.js
@@ -9,135 +9,156 @@ function dconHome() {
 
 function cyberPatriot(){
     var Title = "Cyber Patriot Club"
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function baseball() {
     var Title = "Boys Baseball";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 
 }
 
 function boysBasketball() {
     var Title = "Boys Basketball";
-    var Description = "";
+    var Description = "Work in progress...";
 	document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function boysFootball() {
     var Title = "Boys Football";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function boysLacrosse() {
     var Title = "Boys Lacrosse";
-    var Description = "";
+    var Description = "Work in progress...";
 	var clubImage = ".\clubImages"+Title+".png";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function boysIceHockey() {
     var Title = "Boys Ice Hockey";
-    var Description = "";
+    var Description = "Work in progress...";
 	var clubImage = ".\clubImages"+Title+".png";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function boysSoccer() {
     var Title = "Boys Soccer";
-    var Description = "";
+    var Description = "Work in progress...";
 	var clubImage = ".\clubImages"+Title+".png";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function boysVolleyball() {
     var Title = "Boys Volleyball";
-    var Description = "";
+    var Description = "Work in progress...";
 	var clubImage = ".\clubImages"+Title+".png";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function boysWrestling() {
     var Title = "Boys Wrestling";
-    var Description = "";
+    var Description = "Work in progress...";
 	var clubImage = ".\clubImages"+Title+".png";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function girlsVolleyball() {
     var Title = "Girls Volleyball";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function girlsBasketball() {
     var Title = "Girls Basketball";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function girlsFieldHockey() {
     var Title = "Girls Field Hockey";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function girlsIceHockey() {
     var Title = "Girls Ice Hockey";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function girlsSoccer() {
     var Title = "Girls Soccer";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function softball() {
     var Title = "Softball";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function cheer() {
     var Title = "Cheer";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function crossCountry() {
     var Title = "Cross Country";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function golf() {
     var Title = "Golf";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function bowling() {
     var Title = "Bowling";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function chessclub() {
     var Title = "Chess Club";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 
 function classOf2019() {
     var Title = "Class of 2019";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function classOf2020() {
@@ -149,200 +170,233 @@ function classOf2020() {
 
 function classOf2021() {
     var Title = "Class of 2021";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function classOf2022() {
     var Title = "Class of 2022";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function StudentGov() {
     var Title = "DHS Student Government";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function athleticLeadershipCouncil() {
     var Title = "Athletic Leadership Council";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function dramaClub() {
     var Title = "Dover High School Drama Club";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function earthAction() {
     var Title = "Earth Action";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function equestrian() {
     var Title = "Oyster River - Equestrian Team";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function ffa() {
     var Title = "Future Farmers of America";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function gsa() {
     var Title = "Gay Straight Alliance";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 
 function graniteStateChallenge() {
     var Title = "Granite State Challenge";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function interactClub() {
     var Title = "Interact Club";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function keyClub() {
     var Title = "Key Club";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function mathTeam() {
     var Title = "Math Team";
-    var Description = "";
+    var Description = "<p align=\"center\">Tri-State Math League</p><br>";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function NHS() {
     var Title = "National Honors Society";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function TNHS() {
     var Title = "NH Technical Honors Society";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 
 }
 
 function NJROTC() {
     var Title = "NJROTC";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function photographyClub() {
     var Title = "Photography Club";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function projectSearch() {
     var Title = "Project SEARCH";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function RedCrossClub() {
     var Title = "Red Cross Club";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
 function DSR() {
     var Title = "Dover Shockwave Robotics";
-    var Description = "";
+    var Description = "Work in progress...";
     document.getElementById("ClubName").innerHTML = Title;
+	document.getElementById("Description").innerHTML = Description;
 }
 
     function theTide() {
         var Title = "The Tide";
-        var Description = "";
+        var Description = "Work in progress...";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
 
     function TGC() {
         var Title = "Traditional Gaming Club";
-        var Description = "";
+        var Description = "Work in progress...";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
 
     function WAC() {
         var Title = "World Arts Club";
-        var Description = "";
+        var Description = "Work in progress...";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
 
     function YtY() {
         var Title = "Youth - to - Youth";
-        var Description = "";
+        var Description = "Work in progress...";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
 
     function frenchClub() {
         var Title = "French Club";
-        var Description = "";
+        var Description = "Work in progress...";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
 
     function latinClub() {
         var Title = "Latin Club";
-        var Description = "";
+        var Description = "Work in progress...";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
 
     function spanishClub() {
         var Title = "Spanish Club";
-        var Description = "";
+        var Description = "Work in progress...";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
 
     function percussionEnsemble() {
         var Title = "Percussion Ensemble";
-        var Description = "";
+        var Description = "Work in progress...";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
 
     function colorGuard() {
         var Title = "Marching Band & Color Guard";
-        var Description = "";
+        var Description = "Work in progress...";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
 
     function winterGuard() {
         var Title = "Winter Guard";
-        var Description = "";
+        var Description = "Work in progress...";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
 
     function varietyShow() {
         var Title = "Variety Show";
-        var Description = "";
+        var Description = "Work in progress...";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
 
     function jazzBand() {
         var Title = "Jazz Band";
-        var Description = "";
+        var Description = "Work in progress...";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
 
     function soundWaves() {
         var Title = "Soundwaves";
         var Description = "We get bread";
         document.getElementById("ClubName").innerHTML = Title;
+		document.getElementById("Description").innerHTML = Description;
     }
