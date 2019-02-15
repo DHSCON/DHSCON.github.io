080301e815a5170bd7f9d3cd85a4b2e5518d3b35
Matt-the-Marxist <m.morrissette105000@gmail.com>
2019-02-15 12:31:33 -0500

    search ish

--- new/static/js/control.js
+++ new/static/js/control.js
@@ -1,4 +1,22 @@
-$("#submit").click(function(){
-	console.log("hi");
-	console.log($(".search").text());
+$(document).ready(function(){
+	function Search(query){
+		searchArr = text.split("function");
+		searchArr.shift();
+		let positive = []
+		for(let i=searchArr.length-1;i>0;i--){
+			if(searchArr[i].search(query)==-1){
+				searchArr.splice(i,1);
+				
+			}else{
+				searchArr[i]=searchArr[i].substring(searchArr[i].search("Title")+9)
+			}
+		}
+		searchArr.shift();
+		console.log(searchArr)
+	}
+
+	$("#submit").click(function(){
+		let search = $("#search").val();
+		Search(search);
+	});
 });
