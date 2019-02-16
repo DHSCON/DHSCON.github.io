f45778c2e3efe70ab3c3d114123ea42911782996
Matt-the-Marxist <m.morrissette105000@gmail.com>
2019-02-15 22:57:56 -0500

    search function is somewhat running

--- new/static/js/control.js
+++ new/static/js/control.js
@@ -4,15 +4,46 @@ $(document).ready(function(){
 		searchArr.shift();
 		let positive = []
 		for(let i=searchArr.length-1;i>0;i--){
-			if(searchArr[i].search(query)==-1){
+			regex = new RegExp(query,"i")
+			//negative
+			if(searchArr[i].search(regex)==-1){
 				searchArr.splice(i,1);
-				
+			//positive	
 			}else{
-				searchArr[i]=searchArr[i].substring(searchArr[i].search("Title")+9)
+				console.log(searchArr[i]);
+				function Title(str){
+					let getTitle = str;
+					getTitle=getTitle.substring(getTitle.search("Title")+9);
+					getTitle = getTitle.substring(0,getTitle.search("\""));
+					return getTitle;
+				}
+				function description(str){
+					let getDes = str;
+					getDes=getDes.substring(getDes.search("Description")+15);
+					getDes = getDes.substring(0, getDes.search("\""));
+					return getDes;
+				}
+				function getFunc(str){
+					let clubFunction = str;
+					clubFunction=clubFunction.trim();
+					clubFunction = clubFunction.substring(0, clubFunction.search(/\s/));
+					return clubFunction;
+				}
+
+				positive.push({
+					"title": Title(searchArr[i]),
+					"description": description(searchArr[i]),
+					"function": getFunc(searchArr[i])
+				})
 			}
 		}
 		searchArr.shift();
-		console.log(searchArr)
+		console.log(positive)
+		$("#ClubName").text("Search Results:")
+		for(let i=0;i<positive.length;i++){
+			$(".content").append(`<div id=\"searchResult${i}\"><\/div>`);
+			$(`#searchResult${i}`).append(`<a onclick=\"${positive[i].function}\" href=\"#\">${positive[i].title}<\/a>`)
+		}
 	}
 
 	$("#submit").click(function(){
