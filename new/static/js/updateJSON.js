f8f5277b85bcd178bb9b0dc75974c6192aafccb8
Matt-the-Marxist <m.morrissette105000@gmail.com>
2019-04-26 11:14:58 -0400

    changed search bar; worked on email functionality

--- new/static/js/updateJSON.js
+++ new/static/js/updateJSON.js
@@ -2,12 +2,12 @@ clubs.pages[(GET INDEX OF CLUB)].description = `
 
 `//put description between tildes 
 
-clubs = JSON.stringify(clubs)
+update = JSON.stringify(clubs)
 
 $.ajax({
     url:"https://api.myjson.com/bins/xqszu",
     type:"PUT",
-    data: clubs,
+    data: update,
     contentType:"application/json; charset=utf-8",
     dataType:"json",
     success: function(data, textStatus, jqXHR){
